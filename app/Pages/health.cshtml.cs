3cd9ca12f12b51e5097c144edf58a2cd9d7c5bde
Tomaz Zazijal <tomaz.zazijal@gmail.com>
2024-04-18 13:47:37 +0200

    feat(be): add /health path

---
+++ app/Pages/health.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace HeatWave.Pages
+{
+    class Health
+    {
+        public string Status { get; set; }
+    }
+
+    [AllowAnonymous]
+    public class HealthModel : PageModel
+    {
+        public JsonResult OnGet()
+        {
+            return new JsonResult(JsonConvert.SerializeObject(new Health() { Status = "ok" }));
+        }
+    }
+}
--- app/Pages/history.cshtml.cs
+++ app/Pages/history.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HeatWave.Pages
 {
+    [Authorize]
     public class HistoryModel : PageModel
     {
         public void OnGet()
